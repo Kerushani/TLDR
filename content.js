8cd20850efddbc9b3f2db9c01685f7ac8ecd0d01
Kerushani Sivaneswaran <99515111+Kerushani@users.noreply.github.com>
2025-08-12 08:26:38 -0400

    small changes

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         console.log("Text being sent to BART:", text);
 
         if (text.length > 0) {
-            fetch("http://localhost:5000/summarize", {
+            fetch("http://localhost:3000/summarize", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ text }),
